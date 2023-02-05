ce9f0221b5bd68d7aa707e7fc1014567bbee991c
Supun Wijesooriya <81010139+supunxiii@users.noreply.github.com>
2023-02-05 16:04:04 +0530

    Add files via upload

---
+++ FirstQuestion.java
@@ -0,0 +1,9 @@
+public class FirstQuestion{
+  public static void main(String[] args){
+    String river = "Mississippi";
+    String bigRiver = river.toUpperCase();
+    System.out.println(bigRiver);
+    String newWord = bigRiver.toLowerCase();
+    System.out.println(newWord);
+ }
+}
